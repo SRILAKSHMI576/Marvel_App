069b90c1b69cac79f84ebf5853ae10738c8833dd
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-24 12:00:34 +0530

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        Looking for a <br />
+        lollipop alternative?
+      </h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        <br />
+        industry. Lorem Ipsum has been the industry's standard dummy <br />
+        text ever since the 1500s.
+      </p>
+      <div>
+        <button className="primary">Primary CTA</button>
+        <button className="secondary">Secondary CTA</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
