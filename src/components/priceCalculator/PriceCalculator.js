94d589ff85c387a54173ae75b5db6c6e314dd3e0
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-24 18:40:33 +0530

    Update priceCalculator

--- src/components/priceCalculator/PriceCalculator.js
+++ src/components/priceCalculator/PriceCalculator.js
@@ -3,29 +3,17 @@ import "./style.css";
 
 class PriceCalculator extends React.Component {
   state = {
-    count: 0,
-    monthly_lollipop: 0,
-    monthly_icecandy: 0,
-    monthly_savings: 0,
-    yearly_lollipop: 0,
-    yearly_icecandy: 0,
-    yearly_savings: 0,
+    lollipop: 0,
+    icecandy: 0,
+    savings: 0,
     isChecked: false
   };
 
   hangleClick = number => {
     this.setState({
-      monthly_lollipop: number,
-      monthly_icecandy: 2 * number,
-      monthly_savings: 2 * number - number
-    });
-  };
-
-  hangleClicking = value => {
-    this.setState({
-      yearly_lollipop: value,
-      yearly_icecandy: 2 * value,
-      yearly_savings: (2 * value - value) / 2
+      lollipop: number,
+      icecandy: 2 * number,
+      savings: 2 * number - number
     });
   };
 
@@ -39,18 +27,17 @@ class PriceCalculator extends React.Component {
     if (this.state.isChecked) {
       result = (
         <div>
-          yearly_lollipop: {this.state.yearly_lollipop} <br />
-          yearly_Icecandy: {this.state.yearly_icecandy} <br />
-          yearly_your savings with lollipop: {this.state.yearly_savings} (50%
-          off)
+          yearly_lollipop: {this.state.lollipop / 2} <br />
+          yearly_Icecandy: {this.state.icecandy / 2} <br />
+          yearly_your savings with lollipop: {this.state.savings / 2}
         </div>
       );
     } else {
       result = (
         <div>
-          monthly_lollipop: {this.state.monthly_lollipop} <br />
-          monthly_Icecandy: {this.state.monthly_icecandy} <br />
-          monthly_your savings with lollipop: {this.state.monthly_savings}
+          monthly_lollipop: {this.state.lollipop} <br />
+          monthly_Icecandy: {this.state.icecandy} <br />
+          monthly_your savings with lollipop: {this.state.savings}
         </div>
       );
     }
@@ -58,14 +45,16 @@ class PriceCalculator extends React.Component {
       <div>
         <h3 className="compare">Compare pricing today!</h3>
         <p className="lorem-ipsum-is-simpl">
-          Lorem Ipsum is simply dummy text of the printing and typesetting.
+          Lorem Ipsum is simply dummy text of the printing and
+          <a href="http://marvel.app" className="type">
+            typesetting.
+          </a>
         </p>
-        <div>
+        <div className="buttons">
           <button
             className="rectangle"
             onClick={() => {
               this.hangleClick(25);
-              this.hangleClicking(25);
             }}
           >
             25 Serving
@@ -74,7 +63,6 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(50);
-              this.hangleClicking(50);
             }}
           >
             50 Serving
@@ -83,7 +71,6 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(100);
-              this.hangleClicking(100);
             }}
           >
             100 Serving
@@ -92,7 +79,6 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(200);
-              this.hangleClicking(200);
             }}
           >
             200 Serving
