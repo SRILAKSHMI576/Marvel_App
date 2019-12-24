dc57e64c01bbee244732322f9baca27e88951bb3
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-24 14:31:28 +0530

    Finish info part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header/Header";
+import InfoComponent from "./components/info/InfoComponent";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <InfoComponent />
     </div>
   );
 }
---
+++ src/components/info/InfoComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+function InfoComponent() {
+  return (
+    <div>
+      <div className="info">
+        <div className="BG">
+          <img
+            className="bg"
+            src={require("../../assets/BG.png")}
+            alt="background"
+          />
+        </div>
+
+        <div className="Group">
+          <img
+            className="group"
+            src={require("../../assets/Group 6.png")}
+            alt="group"
+          />
+        </div>
+      </div>
+      <h1 className="heading">
+        This is a heading in <br /> two lines
+      </h1>
+    </div>
+  );
+}
+
+export default InfoComponent;
