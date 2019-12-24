027b6bd088eace930141b80c4b91dd9430df3cc3
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-24 16:47:08 +0530

    Write price Calculations for yearly

--- src/components/priceCalculator/PriceCalculator.js
+++ src/components/priceCalculator/PriceCalculator.js
@@ -4,20 +4,56 @@ import "./style.css";
 class PriceCalculator extends React.Component {
   state = {
     count: 0,
-    lollipop: 0,
-    icecandy: 0,
-    savings: 0
+    monthly_lollipop: 0,
+    monthly_icecandy: 0,
+    monthly_savings: 0,
+    yearly_lollipop: 0,
+    yearly_icecandy: 0,
+    yearly_savings: 0,
+    isChecked: false
   };
 
   hangleClick = number => {
     this.setState({
-      lollipop: number,
-      icecandy: 2 * number,
-      savings: 2 * number - number
+      monthly_lollipop: number,
+      monthly_icecandy: 2 * number,
+      monthly_savings: 2 * number - number
     });
   };
 
+  hangleClicking = value => {
+    this.setState({
+      yearly_lollipop: value,
+      yearly_icecandy: 2 * value,
+      yearly_savings: (2 * value - value) / 2
+    });
+  };
+
+  handleChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked
+    });
+  };
   render() {
+    var result;
+    if (this.state.isChecked) {
+      result = (
+        <div>
+          yearly_lollipop: {this.state.yearly_lollipop} <br />
+          yearly_Icecandy: {this.state.yearly_icecandy} <br />
+          yearly_your savings with lollipop: {this.state.yearly_savings} (50%
+          off)
+        </div>
+      );
+    } else {
+      result = (
+        <div>
+          monthly_lollipop: {this.state.monthly_lollipop} <br />
+          monthly_Icecandy: {this.state.monthly_icecandy} <br />
+          monthly_your savings with lollipop: {this.state.monthly_savings}
+        </div>
+      );
+    }
     return (
       <div>
         <h3 className="compare">Compare pricing today!</h3>
@@ -29,6 +65,7 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(25);
+              this.hangleClicking(25);
             }}
           >
             25 Serving
@@ -37,6 +74,7 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(50);
+              this.hangleClicking(50);
             }}
           >
             50 Serving
@@ -45,6 +83,7 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(100);
+              this.hangleClicking(100);
             }}
           >
             100 Serving
@@ -53,16 +92,16 @@ class PriceCalculator extends React.Component {
             className="rectangle"
             onClick={() => {
               this.hangleClick(200);
+              this.hangleClicking(200);
             }}
           >
             200 Serving
           </button>
         </div>
         <div>
-          lollipop: {this.state.lollipop} <br />
-          Icecandy: {this.state.icecandy} <br />
-          your savings with lollipop: {this.state.savings}
+          <input type="checkbox" onChange={this.handleChange} />
         </div>
+        {result}
       </div>
     );
   }
