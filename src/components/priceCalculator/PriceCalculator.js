2e960a004a906c432a95bf3590de4b73063d9da0
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-27 11:55:27 +0530

    Apply styles for toggle

--- src/components/priceCalculator/PriceCalculator.js
+++ src/components/priceCalculator/PriceCalculator.js
@@ -1,46 +1,48 @@
 import React from "react";
 import "./style.css";
+import Switch from "react-switch";
 
 class PriceCalculator extends React.Component {
   state = {
     lollipop: 0,
     icecandy: 0,
     savings: 0,
-    isChecked: false
+    checked: false,
+    number: 0
+  };
+
+  updatePrices = () => {
+    const div = this.state.checked ? 2 : 1;
+    this.setState({
+      lollipop: this.state.number / div,
+      icecandy: (2 * this.state.number) / div,
+      savings: (2 * this.state.number - this.state.number) / div
+    });
   };
 
   hangleClick = number => {
-    this.setState({
-      lollipop: number,
-      icecandy: 2 * number,
-      savings: 2 * number - number
-    });
+    this.setState(
+      {
+        number: number
+      },
+      () => {
+        this.updatePrices();
+      }
+    );
   };
 
-  handleChange = () => {
-    this.setState({
-      isChecked: !this.state.isChecked
-    });
+  handleChange = checked => {
+    this.setState(
+      {
+        checked: checked
+      },
+      () => {
+        this.updatePrices();
+      }
+    );
   };
+
   render() {
-    var result;
-    if (this.state.isChecked) {
-      result = (
-        <div>
-          yearly_lollipop: {this.state.lollipop / 2} <br />
-          yearly_Icecandy: {this.state.icecandy / 2} <br />
-          yearly_your savings with lollipop: {this.state.savings / 2}
-        </div>
-      );
-    } else {
-      result = (
-        <div>
-          monthly_lollipop: {this.state.lollipop} <br />
-          monthly_Icecandy: {this.state.icecandy} <br />
-          monthly_your savings with lollipop: {this.state.savings}
-        </div>
-      );
-    }
     return (
       <div>
         <h3 className="compare">Compare pricing today!</h3>
@@ -84,10 +86,47 @@ class PriceCalculator extends React.Component {
             200 Serving
           </button>
         </div>
-        <div>
-          <input type="checkbox" onChange={this.handleChange} />
+        <div className="toggle">
+          <div>
+            <label className="annual">Annual</label>
+            <label> 50% off</label>
+          </div>
+          <div>
+            <Switch
+              onChange={this.handleChange}
+              checked={this.state.checked}
+              onColor={"#0099FF"}
+              width={45}
+              height={20}
+            />
+          </div>
+          <label className="monthly">Monthly</label>
         </div>
-        {result}
+
+        <div>
+          {
+            <div className="boxes">
+              <div className="rectangle-copy-17">
+                <label>lollipop:</label>
+                <div className="dollar-copy">${this.state.lollipop}</div>
+                <p className="body-para"> For 100 serving</p>
+              </div>
+              <div className="rectangle-copy-17">
+                <label>Icecandy: </label>
+                <div className="dollar-copy"> ${this.state.icecandy}</div>
+                <p className="body-para"> For 100 serving</p>
+              </div>
+              <div className="rectangle-copy-19">
+                <label>
+                  your savings <br />
+                  with lollipop:
+                </label>
+                <div className="dollar-copy-savings">${this.state.savings}</div>
+                <p className="body-copy-37"> For month</p>
+              </div>
+            </div>
+          }
+        </div>
       </div>
     );
   }
