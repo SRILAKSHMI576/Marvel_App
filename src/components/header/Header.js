a29d56deee12e8663f98f77a0d03b580ff666e4b
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-27 15:17:48 +0530

    Apply responsive for the screen size between 320 and 479

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,9 +10,10 @@ function Header() {
       </h1>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
         <br />
-        industry. Lorem Ipsum has been the industry's standard dummy <br />
-        text ever since the 1500s.
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s.
       </p>
       <div>
         <button className="primary">Primary CTA</button>
