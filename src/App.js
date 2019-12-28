ae5eaa8cd060bc80802e4d607d94ade9ae5968db
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-12-28 23:17:12 +0530

    Update styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Product from "./components/product/product";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <InfoComponent />
       <PriceCalculator />
